2379e9233d9641b6d8e18a840a7fe92b56cde72f
Evgeny Galitskiy <gha665@gmail.com>
2019-12-02 21:44:13 -0500

    created html, basic styling and game board in js file

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log('hello');
+
+window.onload = function () {
+
+    let mole = document.createElement('div');
+    mole.classList.add('mole');
+
+
+    function sendTheMole() {
+        mole.remove();
+        let randomNumWithGlobalScope = (Math.floor(Math.random() * 2000));
+
+        if (randomNumWithGlobalScope > 1500 || randomNumWithGlobalScope < 250) {
+            setTimeout(sendTheMole, randomNumWithGlobalScope)
+            return;
+        }
+        // right now the mole simply appears in a different box once every second
+        // instead, let's make it so the mole sometimes stays for longer than a second
+        // sometimes for less
+        // and sometimes, dissappears and there is no mole for one or two seconds
+
+
+        let boxes = document.getElementsByClassName('col');
+        let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
+
+        randomBox.append(mole);
+
+        setTimeout(sendTheMole, randomNumWithGlobalScope)
+    }
+
+    sendTheMole();
+
+    console.log(document.getElementById('game-board'))
+
+    mole.onclick = function () {
+        alert('you clicked the mole')
+        
+    }
+} // end window onload
+
+
+
+// console.log('hello');
+
+// window.onload = function(){
+
+//     let mole = document.createElement('div');
+//     mole.classList.add('mole');
+
+
+//     console.log(document.getElementById('game-board'))
+
+
+// }// end window onload
