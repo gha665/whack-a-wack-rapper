c7e72fa5734c6c928bf72589390050e1ea454c22
Evgeny Galitskiy <gha665@gmail.com>
2019-12-03 23:58:41 -0500

    updated score function, added edited pictures

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-console.log('hello');
+console.log("hello");
 
 window.onload = function () {
 
-    let mole = document.createElement('div');
-    mole.classList.add('mole');
+  let explosion = document.createElement("img");
+  explosion.setAttribute("src", "theexplosion.gif");
+  explosion.style.width = "50px";
+
+  let theScore = 20;
+
+  let didTheMoleGetWhacked = true;
 
 
-    function sendTheMole() {
-        mole.remove();
-        let randomNumWithGlobalScope = (Math.floor(Math.random() * 2000));
+  let mole = document.createElement("div");
+  mole.classList.add("mole");
 
-        if (randomNumWithGlobalScope > 1500 || randomNumWithGlobalScope < 250) {
-            setTimeout(sendTheMole, randomNumWithGlobalScope)
-            return;
-        }
-        // right now the mole simply appears in a different box once every second
-        // instead, let's make it so the mole sometimes stays for longer than a second
-        // sometimes for less
-        // and sometimes, dissappears and there is no mole for one or two seconds
+  let randomBox;
 
 
-        let boxes = document.getElementsByClassName('col');
-        let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
 
-        randomBox.append(mole);
+  function showTheMole() {
 
-        setTimeout(sendTheMole, randomNumWithGlobalScope)
+    if (!didTheMoleGetWhacked) {
+      theScore -= 10;
     }
 
-    sendTheMole();
 
-    console.log(document.getElementById('game-board'))
+    updateScore();
+    mole.remove();
 
-    mole.onclick = function () {
-        alert('you clicked the mole')
-        
+
+    let randomNumWithGlobalScope = Math.floor(Math.random() * 2000);
+
+    if (randomNumWithGlobalScope > 1500 || randomNumWithGlobalScope < 250) {
+      setTimeout(sendTheMole, randomNumWithGlobalScope);
+      return;
     }
-} // end window onload
+    // right now the mole simply appears in a different box once every second
+    // instead, let's make it so the mole sometimes stays for longer than a second
+    // sometimes for less
+    // and sometimes, dissappears and there is no mole for one or two seconds
 
+    let boxes = document.getElementsByClassName("col");
+    let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
 
+    randomBox.append(mole);
 
-// console.log('hello');
+    setTimeout(showTheMole, randomNumWithGlobalScope);
+  }
 
-// window.onload = function(){
+  showTheMole();
 
-//     let mole = document.createElement('div');
-//     mole.classList.add('mole');
+  console.log(document.getElementById("game-board"));
 
-
-//     console.log(document.getElementById('game-board'))
-
-
-// }// end window onload
+  mole.onclick = function () {
+    alert("you clicked the mole");
+  };
+}; // end window onload
