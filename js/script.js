054d9385b5104332e8f39a1ee13d0c57edafd8b7
gluebag <yezzirmoney@gmail.com>
2019-12-09 23:56:38 -0500

    design changes

--- js/script.js
+++ js/script.js
@@ -1,57 +1,79 @@
 window.onload = function () {
 
-  let theScore = 20;
+    let theScore = 0;
 
-  let randomBox;
-  let rapper;
+    let randomBox;
+    let rapper;
+    let paused = false;
 
-  let audioWin = new this.Audio(
-    './sounds/Yeah-Boy.mp3');
+    let audioWin = new this.Audio(
+        './sounds/Yeah-Boy.mp3');
 
-  function showTheRapper() {
+    function showTheRapper() {
 
-    rapperUpdate();
-    rapperImg1.remove();
+        if(!paused) {
+            rapperUpdate();
+            rapperImg1.remove();
 
-    let randomNumWithGlobalScope = (Math.floor(Math.random() * 4000));
+            let boxes = document.getElementsByClassName('col');
+            randomBox = boxes[Math.floor(Math.random() * boxes.length)];
 
-    let boxes = document.getElementsByClassName('col');
-    randomBox = boxes[Math.floor(Math.random() * boxes.length)];
+            rapperImg2.remove();
+            randomBox.append(rapperImg1);
+        }
 
-
-    rapperImg2.remove();
-    randomBox.append(rapperImg1);
-
-    setTimeout(showTheRapper, randomNumWithGlobalScope);
-  }
-  showTheRapper();
-
-
-  rapperImg1.onclick = function () {
-
-    if (rappers[randomIndex].title === 'wack') {
-      theScore += 10;
-    } else theScore -= 10;
-
-    updateScore();
-    rapperImg1.remove();
-    randomBox.append(rapperImg2);
-  }
-
-  function updateScore() {
-    document.getElementById('score').innerHTML = "Score: " + theScore;
-    checkStatus();
-  }
-
-  function checkStatus() {
-    if (theScore <= 0) {
-      alert('YOU WACK! CULTURE UP!');
+        let randomNumWithGlobalScope = (Math.floor(Math.random() * 4000));
+        setTimeout(showTheRapper, randomNumWithGlobalScope);
     }
 
-    if (theScore => 100) {
-      audioWin.play();
-      alert('KEEP IT REAL!!!');
+    showTheRapper();
+
+
+    rapperImg1.onclick = function () {
+
+        paused = true;
+
+        // resume the  game...
+        setTimeout(function() {
+            if(gameover() === false) {
+                paused = false;
+                document.getElementById("score").style.color = 'white';
+            }
+        }, 2000);
+
+        let scoreColor = 'red';
+
+        if (rappers[randomIndex].title === 'wack') {
+            theScore += 10;
+            scoreColor = 'green';
+        } else theScore -= 10;
+
+        document.getElementById("score").style.color = scoreColor;
+        updateScore();
+        rapperImg1.remove();
+        randomBox.append(rapperImg2);
     }
-  }
+
+    function updateScore() {
+        document.getElementById('score').innerHTML = "Score: " + theScore;
+        checkStatus();
+    }
+
+    function gameover() {
+        return theScore <= 0 || theScore >= 100;
+    }
+
+    function checkStatus() {
+        if (theScore <= 0) {
+            document.getElementById("score").style.color = 'red';
+            document.getElementById('score').innerHTML = "YOU WACK! CULTURE UP!";
+        }
+
+        if (theScore >= 100) {
+            audioWin.play();
+            document.getElementById("score").style.color = 'green';
+            document.getElementById('score').innerHTML = "KEEP IT REAL!!!";
+        }
+    }
 
 }
