19032ce84a5b0312c9161bd5bd5e473dd388f069
Evgeny Galitskiy <gha665@gmail.com>
2019-12-15 20:44:27 -0500

    added start page with instruction, disclaimer and start button, added custom cursor

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
-window.onload = function () {
+document.getElementById('startGame').onclick = () => {
+
+
+
+    document.getElementById("game").style.display = "block";
+    document.getElementById("start").style.display = "none";
+
+
+    // // swap out the cursor...
+    // document.getElementById("game").style.cursor = "hand"; //url('./images/microphone-1.png'), default;";
+    // alert('set custom cursor');
 
     let theScore = 0;
 
     let randomBox;
-    let rapper;
+
     let paused = false;
 
-    let audioWin = new this.Audio(
-        './sounds/Yeah-Boy.mp3');
 
-    let audioLose = new this.Audio(
-        './sounds/so-damn-tough.mp3');
 
+
+    //---------------------sound decoration---------------------------------//
+    let audioWin = new Audio('./sounds/Yeah-Boy.mp3');
+    let audioLose = new Audio('./sounds/so-damn-tough.mp3');
+    let audioPunch = new Audio('./sounds/punch.mp3')
+    let audioBackground = new Audio();
+    audioBackground.src = './sounds/instrumental.mp3';
+    document.querySelector('body').append(audioBackground);
+    audioBackground.play();
+
+    // audioBackground.onload = () => {
+    //     audioBackground.play();
+    // }
+
+
+
+    //-----------------------shows rappers' images-------------------------//
     function showTheRapper() {
 
         if (!paused) {
@@ -25,14 +48,17 @@ window.onload = function () {
             randomBox.append(rapperImg1);
         }
 
-        let randomNumWithGlobalScope = (Math.floor(Math.random() * 3000));
+        let randomNumWithGlobalScope = (Math.floor(Math.random() * 4000));
         setTimeout(showTheRapper, randomNumWithGlobalScope);
     }
-
     showTheRapper();
 
 
+
+    //-----------------------actions when you click rappers' images---------//
     rapperImg1.onclick = function () {
+
+        audioPunch.play();
 
         paused = true;
 
@@ -57,27 +83,48 @@ window.onload = function () {
         randomBox.append(rapperImg2);
     }
 
+
+
+    //------------------------updates the score-------------------------//
     function updateScore() {
-        document.getElementById('score').innerHTML = "Score: " + theScore;
+        document.getElementById("score").innerHTML = "$core: " + theScore;
         checkStatus();
     }
 
+
+
+    //------------------------ends the game-----------------------------//
     function gameover() {
         return theScore <= 0 || theScore >= 100;
     }
 
+
+
+    //------------------------when the game ends------------------------// 
     function checkStatus() {
         if (theScore <= 0) {
+            audioBackground.pause();
             audioLose.play();
             document.getElementById("score").style.color = 'red';
-            document.getElementById('score').innerHTML = "YOU'RE WACK! CULTURE UP!";
+            document.getElementById("score").innerHTML = "YOU'RE WACK! CULTURE UP!";
         }
 
         if (theScore >= 100) {
+            audioBackground.pause();
             audioWin.play();
             document.getElementById("score").style.color = 'green';
-            document.getElementById('score').innerHTML = "KEEP IT REAL!!!";
+            document.getElementById("score").innerHTML = "KEEP IT REAL, PLAYA!!!";
         }
     }
 
 }
+
+
+//------------------------ swap cursor on click -----------------------// 
+$('#game').on('mousedown', function(e) {
+    $('#game').addClass('cursor-clicked');
+});
+
+$('#game').on('mouseup', function(e) {
+    $('#game').removeClass('cursor-clicked');
+});
